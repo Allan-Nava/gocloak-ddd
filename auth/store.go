720ff1d04a034ab51f6981ab9a3279bfd59722d8
Allan Nava <allannava95@gmail.com>
2022-02-08 17:30:03 +0100

    updated gocloak

--- auth/store.go
+++ auth/store.go
@@ -1,39 +1,38 @@
 package auth
-
 /*
 * Copyright © 2022 Allan Nava <>
 * Created 08/02/2022
 * Updated 08/02/2022
 *
  */
+
 import (
 	"fmt"
 
 	"github.com/Allan-Nava/gocloak-ddd/config"
 )
 
-
-type AuthService struct {
+type AuthStore interface {
+	Login( username string, password string ) // need to add jwt 
 }
 
+
+type GoCloakAuthStore struct {
+	GCloakClient *gocloak.GoCloak
+
+}
 //
-func (s *AuthService) Login(username string, password string) (*AuthResponse, error) {
+func (s *GoCloakAuthStore) Login(username string, password string ) {
+	//
 	Client := config.CONFIGURATION.KeycloakClientId
 	ClientSecret := config.CONFIGURATION.KeycloakSecret
 	Realm := config.CONFIGURATION.KeycloakRealm
 	//
-	fmt.Printf("Client %v | ClientSecret %v | Realm %v", Client, ClientSecret, Realm)
-	return nil, nil
-	/*jwt, err := GCloakClient.Login(Client, ClientSecret, Realm, username, password)
+	jwt, err := GCloakClient.Login(Client, ClientSecret, Realm, username, password)
 
 	if err != nil {
 		// need to change the status code 400
 		return nil, err
 	}
-	var resp = AuthResponse{Token: jwt.}
-
-	return &resp, nil*/
-
+	return nil, nil
 }
-
-//
